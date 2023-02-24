48144d9596f4e214279d481718621a9464eecff2
olivermnoo <riseumpro@gmail.com>
2023-02-24 00:50:46 -0300

    Melhoria loading da página de detalhes do filme

--- src/pages/MoviePage/index.jsx
+++ src/pages/MoviePage/index.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { HeaderContentPage } from "../../components/HeaderContentPage";
@@ -11,35 +11,48 @@ export function MoviePage() {
   const [movie, setMovie] = useState({});
   const [similarMovies, setSimilarMovies] = useState();
   const [isLoading, setIsLoading] = useState(false);
+  const [load1, setLoad1] = useState(false);
+  const [load2, setLoad2] = useState(false);
   const languageSelect = useLanguage((state) => state.language);
+  const navigate = useNavigate();
 
   useEffect(() => {
     async function fetchMovie() {
-      const res = await axios.get(
-        `https://api.themoviedb.org/3/movie/${params.id}?api_key=9b0bf59083345bf6f6a1b1a347761971&language=${languageSelect}`
-      );
-
-      setMovie(res.data);
+      try {
+        const res = await axios.get(
+          `https://api.themoviedb.org/3/movie/${params.id}?api_key=9b0bf59083345bf6f6a1b1a347761971&language=${languageSelect}`
+        );
+        setMovie(res.data);
+        setLoad1(true);
+      } catch (err) {
+        console.log(err);
+        navigate("/");
+      }
     }
     fetchMovie();
+
     async function fetchSimilarMovies() {
-      const res = await axios.get(
-        `https://api.themoviedb.org/3/movie/${params.id}/recommendations?api_key=9b0bf59083345bf6f6a1b1a347761971&language=${languageSelect}`
-      );
-      //abaixo filtrando apenas filmes semelhantes que possuem imagem, descrição e ordenando por popularidade
-      const similarMoviesFiltered = res.data.results
-        .filter((e) => e.backdrop_path !== null && e.overview)
-        .sort((a, b) => Number(a.popularity) > Number(b.popularity));
-      setSimilarMovies(similarMoviesFiltered);
-      console.log(similarMoviesFiltered);
-      setIsLoading(true);
+      try {
+        const res = await axios.get(
+          `https://api.themoviedb.org/3/movie/${params.id}/recommendations?api_key=9b0bf59083345bf6f6a1b1a347761971&language=${languageSelect}`
+        );
+        //abaixo filtrando apenas filmes semelhantes que possuem imagem, descrição e ordenando por popularidade
+        const similarMoviesFiltered = res.data.results
+          .filter((e) => e.backdrop_path !== null && e.overview)
+          .sort((a, b) => Number(a.popularity) > Number(b.popularity));
+        setSimilarMovies(similarMoviesFiltered);
+        setLoad2(true);
+      } catch (err) {
+        console.log(err);
+        navigate("/");
+      }
     }
     fetchSimilarMovies();
   }, [languageSelect, params]);
 
   return (
     <>
-      {isLoading && (
+      {load1 && load2 && (
         <motion.div
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
@@ -49,7 +62,7 @@ export function MoviePage() {
           <HeaderContentPage movie={movie} similarMovies={similarMovies} />
         </motion.div>
       )}
-      {!isLoading && <LoadingScreen />}
+      {!load1 && !load2 && <LoadingScreen />}
     </>
   );
 }
